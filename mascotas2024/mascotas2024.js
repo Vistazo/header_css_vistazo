ca5a674e319db3d0017384ff1ac54d18ef49fb4b
Vistazo <150281411+Vistazo@users.noreply.github.com>
2024-06-18 11:07:21 -0500

    Update mascotas2024.js

--- mascotas2024/mascotas2024.js
+++ mascotas2024/mascotas2024.js
@@ -7,6 +7,19 @@ function toggleMenu() {
   }
 }
 
+// Obtener todas las etiquetas <a> dentro de la clase .menuIcon
+var links = document.querySelectorAll('.menuIcon a');
+
+// Cambiar el href de la primera etiqueta <a>
+if (links.length > 0) {
+    links[0].href = "https://www.instagram.com/mascotasdigital";
+}
+
+// Cambiar el href de la segunda etiqueta <a>
+if (links.length > 1) {
+    links[1].href = "https://www.tiktok.com/@mascotasdigital.ec";
+}
+
 
 
 function searchMenu() {
