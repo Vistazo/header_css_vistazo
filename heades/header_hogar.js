56dcf5f3352eabb156e9a931a4fd9840e77f7cf1
Vistazo <150281411+Vistazo@users.noreply.github.com>
2024-07-19 13:23:07 -0500

    Update header_hogar.js

--- heades/header_hogar.js
+++ heades/header_hogar.js
@@ -1,54 +1,59 @@
 document.addEventListener("DOMContentLoaded", function() {
-try {
-    var navContainer = document.querySelector('#menu_1712675375 .parent-nav');
-    // Verificar si navContainer existe
-    if (!navContainer) {
-        throw new Error('El contenedor de navegación no se encontró.');
-    }
-    // Ocultar las opciones originales de "Moda" y "Belleza"
-    var modaItem = navContainer.querySelector('.sect-4c8eef10f077d984f42db516026ee9e5');
-    var bellezaItem = navContainer.querySelector('.sect-35171bd57250da9c6c11190279144063');
+    try {
+        var navContainer = document.querySelector('#menu_1712675375 .parent-nav');
+        console.log("navContainer: ", navContainer); // Agregar salida de depuración
 
+        // Verificar si navContainer existe
+        if (!navContainer) {
+            throw new Error('El contenedor de navegación no se encontró.');
+        }
 
-    if (modaItem) modaItem.style.display = 'none';
-    if (bellezaItem) bellezaItem.style.display = 'none';
-    
-    // Inyectar HTML
-    var tendenciasHTML = `
-        <li class="lst-item tabnav sect-tendencias">
-            <div class="tab-grp">
-                <a href="#" class="tab-item lnk" title="">
-                    <span class="iconBefore"></span>
-                    <strong class="sectionName">Tendencias</strong>
-                    <span class="iconAfter"></span>
-                </a>
-                <span class="hasChildsIcon"></span>
-                <div class="child-nav cf mouseleave">
-                    <ul class="lst-std lst level-2">
-                        <div class="lst-items">
-                            <li class="lst-item sect-belleza">
-                                <a href="/hogar/belleza" class="lnk" title="">
-                                    <span class="iconBefore"></span>
-                                    <span class="sectionName">Belleza</span>
-                                    <span class="iconAfter"></span>
-                                </a>
-                            </li>
-                            <li class="lst-item sect-moda">
-                                <a href="/hogar/moda" class="lnk" title="">
-                                    <span class="iconBefore"></span>
-                                    <span class="sectionName">Moda</span>
-                                    <span class="iconAfter"></span>
-                                </a>
-                            </li>
-                        </div>
-                    </ul>
+        // Ocultar las opciones originales de "Moda" y "Belleza"
+        var modaItem = navContainer.querySelector('.sect-4c8eef10f077d984f42db516026ee9e5');
+        var bellezaItem = navContainer.querySelector('.sect-35171bd57250da9c6c11190279144063');
+
+        if (modaItem) modaItem.style.display = 'none';
+        if (bellezaItem) bellezaItem.style.display = 'none';
+
+        // Inyectar HTML
+        var tendenciasHTML = `
+            <li class="lst-item tabnav sect-tendencias">
+                <div class="tab-grp">
+                    <a href="#" class="tab-item lnk" title="">
+                        <span class="iconBefore"></span>
+                        <strong class="sectionName">Tendencias</strong>
+                        <span class="iconAfter"></span>
+                    </a>
+                    <span class="hasChildsIcon"></span>
+                    <div class="child-nav cf mouseleave">
+                        <ul class="lst-std lst level-2">
+                            <div class="lst-items">
+                                <li class="lst-item sect-belleza">
+                                    <a href="/hogar/belleza" class="lnk" title="">
+                                        <span class="iconBefore"></span>
+                                        <span class="sectionName">Belleza</span>
+                                        <span class="iconAfter"></span>
+                                    </a>
+                                </li>
+                                <li class="lst-item sect-moda">
+                                    <a href="/hogar/moda" class="lnk" title="">
+                                        <span class="iconBefore"></span>
+                                        <span class="sectionName">Moda</span>
+                                        <span class="iconAfter"></span>
+                                    </a>
+                                </li>
+                            </div>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </li>
-    `;
+            </li>
+        `;
 
-    navContainer.insertAdjacentHTML('afterbegin', tendenciasHTML);
-} finally {
-    console.log('Script de inyección de tendencias ejecutado.');
-}
+        navContainer.insertAdjacentHTML('afterbegin', tendenciasHTML);
+
+    } catch (error) {
+        console.error(error.message);
+    } finally {
+        console.log('Script de inyección de tendencias ejecutado.');
+    }
 });
