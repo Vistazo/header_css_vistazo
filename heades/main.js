a7cb786bb8d918dcfdf8b1bee6951546beb6ccd9
DJMarret <javier_dario_marret@hotmail.com>
2024-08-15 11:50:43 -0500

    videoteca clear

--- heades/main.js
+++ heades/main.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
   EjecutarInit();
   if(path == "/" || path == "/tes/index.html"){
     // cambiarImagenPautaHome();
-    // videoTecarender();
+    videoTecarender();
   }
   // if(path == "/deportes" || path == "/" || path == "/deportes/jjoo-2024"){
   //   cambiarImagenPautaEstadio();
