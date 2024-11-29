1787fda1c7775992dbe8a58be85df47d12531b11
DJMarret <javier_dario_marret@hotmail.com>
2024-11-29 17:20:26 -0500

    nitificaciones

--- heades/rediseno.js
+++ heades/rediseno.js
@@ -547,3 +547,28 @@ function mostrarInstruccionesPermiso() {
 
 // Llamar automáticamente a la función al cargar la página
 solicitarPermisoNotificaciones();
+
+
+
+// Activar notificaciones asi la pagina las tenga desactivadas
+async function activarNotificaciones() {
+  // Verificar el estado actual del permiso
+  Notification.requestPermission().then(permission => {
+    console.log("Estado de permisos:", permission);
+      if (permission === "granted") {
+          // Notificaciones ya activadas
+          new Notification("¡Notificaciones activadas con éxito!", {
+              body: "Ahora recibirás notificaciones importantes.",
+              icon: "https://codigomarret.online/upload/img/logo-vis.png" // Reemplaza con la URL del icono de tu aplicación
+          });
+      } else if (permission === "denied") {
+          // Si están denegadas, informa al usuario que debe activarlas manualmente
+          alert("Parece que denegaste las notificaciones. Actívalas desde la configuración de tu navegador.");
+      } else {
+          // Caso "default" o cuando el usuario no interactúa
+          alert("Por favor, habilita las notificaciones para recibir actualizaciones.");
+      }
+  }).catch(error => {
+      console.error("Error al solicitar notificaciones:", error);
+  });
+}
