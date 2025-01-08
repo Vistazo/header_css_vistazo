f2375fe39d883545190253b6fd1dcaf5c233a51c
Eri Vera <eriveraec@gmail.com>
2025-01-07 19:31:33 -0500

    cambios

--- elecciones2025/eleccion.js
+++ elecciones2025/eleccion.js
@@ -60,7 +60,7 @@ function startCountdown(targetDate) {
 }
 
 // Fecha objetivo: 4 de febrero de 2025 a las 00:00:00
-const targetDate = new Date("2025-02-04T00:00:00");
+const targetDate = new Date("2025-02-09T00:00:00");
 
 setTimeout(() => {
   swiperCandidatos();
