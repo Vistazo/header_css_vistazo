88036f60e48e16548c5793ce156d2e6b854f19ba
Eri Vera <eriveraec@gmail.com>
2025-01-11 12:05:39 -0500

    Update eleccion.js

--- elecciones2025/eleccion.js
+++ elecciones2025/eleccion.js
@@ -4,24 +4,24 @@ function swiperCandidatos() {
   var eleSwiper = document.querySelector('.bloque-candidatos');
   var eleWrapper = document.querySelector('.bloque-candidatos .noticias');
   var elementsSlide = document.querySelectorAll('.bloque-candidatos .noticias .article');
-  
+
   eleSwiper.classList.add('swiper');
   eleWrapper.classList.add('swiper-wrapper');
-  
+
   for (const xSliders of elementsSlide) {
     xSliders.classList.add('swiper-slide');
   }
-  
+
   var prev = document.createElement('div');
   var next = document.createElement('div');
-  
+
   prev.classList.add('swiper-button-prev');
   next.classList.add('swiper-button-next');
-  
+
   eleSwiper.appendChild(prev);
   eleSwiper.appendChild(next);
-  
-  
+
+
   var init = new Swiper(eleSwiper, {
     slidesPerView: 1,
     spaceBetween: 30,
@@ -80,7 +80,49 @@ function startCountdown(targetDate) {
 // Fecha objetivo: 4 de febrero de 2025 a las 00:00:00
 const targetDate = new Date("2025-02-09T00:00:00");
 
+const API_URL = "https://vtz.bmcodigo.com/getPortadaElecciones";
+
+async function fetchData() {
+  try {
+    const response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error(`Error en la solicitud: ${response.status}`);
+    }
+
+    const data = await response.json();
+    renderResults(data);
+  } catch (error) {
+    console.error("Error al consumir la API:", error);
+    document.getElementById("results").innerHTML = `<p>Error al cargar los datos.</p>`;
+  }
+}
+
+function renderResults(data) {
+  const resultsContainer = document.getElementById("results");
+
+  data.forEach((item) => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    card.innerHTML = `
+          <img src="${item.src}" alt="${item.title}">
+          <div class="content">
+            <h2>${item.title}</h2>
+            <a href="${item.redirect}" target="_blank">Ir al enlace</a>
+          </div>
+        `;
+
+    resultsContainer.appendChild(card);
+  });
+}
+
+// Llamar a la función para cargar los datos al cargar la página
+
+
+
+
 setTimeout(() => {
+  fetchData();
   swiperCandidatos();
   startCountdown(targetDate);
 }, 500);
