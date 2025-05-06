18099ae7818bae481b40638856f07134d35d3030
DJMarret <javier_dario_marret@hotmail.com>
2025-05-05 20:40:50 -0500

    est

--- heades/rediseno.js
+++ heades/rediseno.js
@@ -294,13 +294,13 @@ function cambiarImagenPautaHome() {
   var enlace = document.querySelector(".IMG_PAUTAS_DIGITALES .multimedia a");
   var imagen = document.querySelector(".IMG_PAUTAS_DIGITALES .multimedia img");
   var imagenesDesktop = [
-    { src: "https://codigomarret.online/upload/img/banner-mascotas-1920x200.jpg", href: "https://www.vistazo.com/mascotas" },
-    // { src: "https://codigomarret.online/upload/img/elecciones-(1920-x-200-px).png", href: "https://www.vistazo.com/elecciones-ecuador-presidente-asamblea-2025" },
+    // { src: "https://codigomarret.online/upload/img/banner-mascotas-1920x200.jpg", href: "https://www.vistazo.com/mascotas" },
+    { src: "https://codigomarret.online/upload/img/banners_china.png", href: "https://www.vistazo.com/politica/investigacion-objetivos-de-china" },
     // Agrega más objetos para más imágenes y enlaces
   ];
   var imagenesMovil = [
-    { src: "https://codigomarret.online/upload/img/banner-mascotas-400x100.jpg", href: "https://www.vistazo.com/mascotas" },
-    // { src: "https://codigomarret.online/upload/img/elecciones-(400-x-100-px).png", href: "https://www.vistazo.com/elecciones-ecuador-presidente-asamblea-2025" },
+    // { src: "https://codigomarret.online/upload/img/banner-mascotas-400x100.jpg", href: "https://www.vistazo.com/mascotas" },
+    { src: "https://codigomarret.online/upload/img/banners_china.png", href: "https://www.vistazo.com/politica/investigacion-objetivos-de-china" },
     // Agrega más objetos para más imágenes y enlaces
   ];
   let indiceActual = 0;
