04ab83fa94ca8f9f1b4f4b45c67ff987fffef0aa
Eri Vera <eriveraec@gmail.com>
2026-03-21 09:17:59 -0500

    Create revistas.js

---
+++ rediseno/revistas.js
@@ -0,0 +1,37 @@
+const slidesData = [
+    {
+        img: "https://codigomarret.online/upload/img/en_circulacion.webp",
+        link: "https://suscripciones.vistazo.com/",
+        edition: "Edición No.1402 · Dic 4 / 2025",
+        title: "Trump Aprieta a Maduro",
+        price: "Suscríbete por $5,41/mes"
+    },
+    {
+        img: "https://codigomarret.online/upload/img/revista_2.png",
+        link: "https://suscripciones.vistazo.com/",
+        edition: "Edición No.1401 · Nov 20 / 2025",
+        title: "La Crisis Económica que Viene",
+        price: "Suscríbete por $5,41/mes"
+    },
+    {
+        img: "https://codigomarret.online/upload/img/revista_3.png",
+        link: "https://suscripciones.vistazo.com/",
+        edition: "Edición No.1400 · Nov 6 / 2025",
+        title: "El Poder de las Mujeres en Política",
+        price: "Suscríbete por $5,41/mes"
+    },
+    {
+        img: "https://codigomarret.online/upload/img/revista_4.png",
+        link: "https://suscripciones.vistazo.com/",
+        edition: "Edición No.1399 · Oct 23 / 2025",
+        title: "Secretos del Narcotráfico Regional",
+        price: "Suscríbete por $5,41/mes"
+    },
+    {
+        img: "https://codigomarret.online/upload/img/revista_5.png",
+        link: "https://suscripciones.vistazo.com/",
+        edition: "Edición No.1398 · Oct 9 / 2025",
+        title: "Tecnología e Innovación en Ecuador",
+        price: "Suscríbete por $5,41/mes"
+    }
+];
