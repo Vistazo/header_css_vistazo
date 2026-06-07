4ab1dfefdb3d7fa1f57000b5d55525ca6cd22edf
DarioMarret <javier_dario_marret@hotmail.com>
2026-06-07 06:32:10 -0500

    es

--- rediseno/ampliada/detalle.js
+++ rediseno/ampliada/detalle.js
@@ -61,6 +61,21 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         imagenBlock.insertAdjacentElement('afterend', relacionadas);
     }
+
+    // Reemplazar imagen de Google por botón
+    const wrapper = document.querySelector('.canal_whatsapp_movil');
+    if (wrapper) {
+        const imgLink = wrapper.querySelector('a:not(.vtz-wsp-card):not(.vtz-google-btn)');
+        if (imgLink) {
+            imgLink.classList.add('vtz-google-news-btn');
+            imgLink.innerHTML =
+                '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">' +
+                '<path stroke="none" d="M0 0h24v24H0z" fill="none"/>' +
+                '<path d="M12 2a9.96 9.96 0 0 1 6.29 2.226a1 1 0 0 1 .04 1.52l-1.51 1.362a1 1 0 0 1 -1.265 .06a6 6 0 1 0 2.103 6.836l.001 -.004h-3.66a1 1 0 0 1 -.992 -.883l-.007 -.117v-2a1 1 0 0 1 1 -1h6.945a1 1 0 0 1 .994 .89c.04 .367 .061 .737 .061 1.11c0 5.523 -4.477 10 -10 10s-10 -4.477 -10 -10s4.477 -10 10 -10z"/>' +
+                '</svg>' +
+                '<span>Agregar en Google</span>';
+        }
+    }
 });
 
 
